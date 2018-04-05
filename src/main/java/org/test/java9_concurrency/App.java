4f6da45c0adfb27b1e9b433eb49cb43d2bf97f9c
Lucky Pratama <luckypratama71@yahoo.com>
2018-04-06 02:22:39 +0700

    Initial commit, project skeleton

---
+++ src/main/java/org/test/java9_concurrency/App.java
@@ -0,0 +1,13 @@
+package org.test.java9_concurrency;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
